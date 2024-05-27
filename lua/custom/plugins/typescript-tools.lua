ab8ae222ddf39843e2a4d2180f55bf4c805f00d4
DeterminationCode1 <89226663+DeterminationCode1@users.noreply.github.com>
2024-05-27 16:39:31 +0200

    Added typescrip-tools lsp plugin

---
+++ lua/custom/plugins/typescript-tools.lua
@@ -0,0 +1,7 @@
+-- Official repo: https://github.com/pmizio/typescript-tools.nvim
+return {
+  'pmizio/typescript-tools.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  opts = {},
+  -- settings = {} -- NOTE: I think settings could be added here, but not sure.
+}
