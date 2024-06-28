94fa64d575c27228442482319ea6439d1abb9043
DeterminationCode1 <89226663+DeterminationCode1@users.noreply.github.com>
2024-06-28 11:25:34 +0200

    Fix lsp-config: changed diagnostic signs

--- init.lua
+++ init.lua
@@ -653,6 +653,14 @@ require('lazy').setup({
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
           end
+
+          -- Me: change the lsp gutter symbols
+          -- see official docs: https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#change-diagnostic-symbols-in-the-sign-column-gutter
+          local signs = { Error = '󰅚 ', Warn = '󰀪 ', Hint = '󰌶 ', Info = ' ' }
+          for type, icon in pairs(signs) do
+            local hl = 'DiagnosticSign' .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+          end
         end,
       })
 
