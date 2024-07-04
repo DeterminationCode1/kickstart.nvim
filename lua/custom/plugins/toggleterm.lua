ea2d431ad890d23dbd141979fb07dbf732aa113b
DeterminationCode1 <89226663+DeterminationCode1@users.noreply.github.com>
2024-07-04 13:18:02 +0200

    Fix toggleterm: keybindings had wrong modes

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -3,7 +3,7 @@
 -- Usage
 -- - Open the terminal with <c-\>
 -- - Close the terminal with <c-\>
--- - Open 3 terminal with 3<c-\>
+-- - Open third terminal with 3<c-\>
 -- - To use normal vim motions in the terminal, you must first exit terminal mode with
 --   <ESC><ESC> as you mapped it to it.
 --
@@ -39,8 +39,10 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {
+    -- WARN: open_mapping caused bugs with mapping being set in insert mode. just defint you own mapping instead
+    --
     -- mnemonic: open terminal. easier to type than <c-\> or <leader>te
-    open_mapping = '<leader>o', -- or { [[<c-\>]],
+    -- open_mapping = '<leader>o', -- or { [[<c-\>]],
     direction = 'tab', -- Default: 'horizontal'
     -- if  open_mappings should be available in insert mode
     insert_mappings = false, -- Default: true
@@ -48,11 +50,24 @@ return {
   config = function(_, opts)
     require('toggleterm').setup(opts)
 
+    vim.keymap.set({ 'n' }, '<leader>o', '<Cmd>execute v:count . "ToggleTerm"<CR>', {
+      desc = 'Toggle Terminal',
+      silent = true,
+    })
     -- Optional, you can create a different mapping for
     vim.keymap.set('i', '<C-o>', '<Esc><Cmd>ToggleTerm<CR>', {
       desc = 'Toggle Terminal',
       silent = true,
     })
+
+    -- NOTE: Me: this was copy pasted from kickstart init.lua
+    --
+    -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+    -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+    -- is not what someone will guess without a bit more experience.
+    --
+    -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+    -- or just use <C-\><C-n> to exit terminal mode
+    vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
   end,
 }
-
