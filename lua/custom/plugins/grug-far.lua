d0158589c6352e19998ad6cb3b991a2b3ff92c22
DeterminationCode1 <89226663+DeterminationCode1@users.noreply.github.com>
2025-01-09 15:22:33 +0100

    feat: grug-far keymap for search only current file

--- lua/custom/plugins/grug-far.lua
+++ lua/custom/plugins/grug-far.lua
@@ -4,7 +4,7 @@
 -- Grug-far is a plugin to simply search and replace text in your whole code
 -- base.
 --
--- Usage:
+-- =============== Usage: ===================================================
 --
 -- <leader>rs: Start the search and replace with your keybinding
 --
@@ -49,7 +49,44 @@ return {
         }
       end,
       mode = { 'n', 'v' },
-      desc = 'Search and Replace',
+      desc = 'Search and Replace ([R]eplace [S]earch with Grug-far)',
     },
   },
+  config = function()
+    -- set up grug-far.nvim
+    require('grug-far').setup {}
+
+    -- Set <localleader> to `,` because its too risky accidentally hit commands
+    -- that replace words in all files if the local leader is `<sapce>`
+    -- config docs grug-far: https://github.com/MagicDuck/grug-far.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
+    -- WARNING: the localleader must be configured before lazy is started.
+    -- vim.g.maplocalleader = ','
+
+    -- Create a buffer local keybinding to open a result location and immediately close grug-far.nvim
+    --
+    -- official docs recipes: https://github.com/MagicDuck/grug-far.nvim?tab=readme-ov-file#create-a-buffer-local-keybinding-to-open-a-result-location-and-immediately-close-grug-farnvim
+    -- me: FIX: doesn't seem to work?
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('grug-far-keybindings', { clear = true }),
+      pattern = { 'grug-far' },
+      callback = function()
+        -- (where <localleader>o and <localleader>c are the default keybindings for Open and Close actions. You will need to change them if you set them to something different)
+        vim.api.nvim_buf_set_keymap(0, 'n', '<C-enter>', '<localleader>o<localleader>c', {})
+      end,
+    })
+
+    -- Launch grug-far with search/replace limited to the  current file
+    --
+    -- official recipes copy pasted: https://github.com/MagicDuck/grug-far.nvim?tab=readme-ov-file#launch-limiting-searchreplace-to-current-file
+    --
+    -- INFO: the default recipe didn't work because I use filename with `spaces`
+    -- in my markdown files that were not automatically escaped correctly and
+    -- cause ripgrep to not find the file.
+    vim.keymap.set('n', '<leader>rf', function()
+      local file_path = vim.fn.expand '%'
+      -- NOTE: my fix to escape spaces in filenames. Replace spaces with '\ '
+      file_path = file_path:gsub(' ', '\\ ')
+      require('grug-far').open { prefills = { paths = file_path } }
+    end, { desc = 'Search and Replace in current file ([R]replace in current [F]ile)' })
+  end,
 }
